fa43fe766762d58232f50e6aff0e7ab67259dcb2
halukbuyukozkan <halukbuyukozkan@gmail.com>
2022-09-15 01:15:09 +0300

    interface created

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\FieldController;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\MoneyRequestController;
 use App\Http\Controllers\MoneyRequestItemController;
 use App\Http\Controllers\PermissionController;
@@ -28,15 +30,15 @@ Route::get('/', function () {
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('index');
-    })->name('dashboard');
 
+    Route::get('dashboard',[FrontController::class,'index'])->name('dashboard');
     Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('front',FrontController::class);
         Route::resource('user',UserController::class);
+        Route::resource('about',AboutController::class);
+        Route::resource('message',MessageController::class);
         Route::resource('role', RoleController::class);
         Route::resource('permission', PermissionController::class);
-        Route::resource('about',AboutController::class);
         Route::resource('field', FieldController::class);
         Route::resource('department',DepartmentController::class);
         Route::resource('moneyrequest',MoneyRequestController::class);
