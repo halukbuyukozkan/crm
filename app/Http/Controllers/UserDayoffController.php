95a1015b411794eeb30823483a6030a22fe8c571
halukbuyukozkan <halukbuyukozkan@gmail.com>
2022-10-17 02:05:11 +0300

    calendar created

---
+++ app/Http/Controllers/UserDayoffController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Dayoff;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Mockery\Generator\StringManipulation\Pass\Pass;
+
+class UserDayoffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function index(User $user)
+    {
+        //
+    }
+
+    public function calendar()
+    {
+        $userdayoffs = Dayoff::all();
+        $dayoffs = null;
+        foreach($userdayoffs as $dayoff){
+            $dayoffs[] = [
+                'title' => $dayoff->title,
+                'start' => $dayoff->start_date,
+                'end' => $dayoff->end_date,
+            ];
+        }
+
+        $user = Auth::user();
+
+        return view('dayoff.calendar',compact('dayoffs','user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $user)
+    {
+        $request->validate([
+            'title' => 'required|string'
+        ]);
+        
+        $dayoff = Dayoff::create([
+            'title' => $request->title,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+        ]);
+
+        return response()->json($dayoff);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Dayoff  $dayoff
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user, Dayoff $dayoff)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Dayoff  $dayoff
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user, Dayoff $dayoff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Dayoff  $dayoff
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user, Dayoff $dayoff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Dayoff  $dayoff
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user, Dayoff $dayoff)
+    {
+        //
+    }
+}
